d8ffc2e5cb00336d1d404de4712448ee901326d6
dannielf <danielgranados1992@gmail.com>
2022-06-25 13:44:51 -0500

    Project started.

---
+++ src/main/java/com/chat/testchat/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.chat.testchat.config;
+
+import org.springframework.context.annotation.Configuration;
+        import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+        import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+        import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * Endpoint topics
+     * @param registry MessageBrokerRegistry
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/chatroom","/user");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    /**
+     * Endpoint Stomp allowing all origins
+     * @param registry StompEndpointRegistry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
