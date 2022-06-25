e5c888effcf17f7bb584caecfa2e1b7ab103de29
dannielf <danielgranados1992@gmail.com>
2022-06-25 14:26:37 -0500

    [Add] Controller UserController, MessageController, ChannelController

---
+++ src/main/java/com/chat/testchat/controller/ChannelController.java
@@ -0,0 +1,15 @@
+package com.chat.testchat.controller;
+
+import com.chat.testchat.usecases.ChannelUseCase;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@AllArgsConstructor
+public class ChannelController {
+
+    private final ChannelUseCase channelUseCase;
+    private Logger logger = LoggerFactory.getLogger(ChannelController.class);
+}
---
+++ src/main/java/com/chat/testchat/controller/MessageController.java
@@ -0,0 +1,15 @@
+package com.chat.testchat.controller;
+
+import com.chat.testchat.usecases.MessageUseCase;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@AllArgsConstructor
+public class MessageController {
+
+    private final MessageUseCase messageUseCase;
+    private Logger logger = LoggerFactory.getLogger(MessageController.class);
+}
